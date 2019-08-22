6c5834dc120036bab85c623005e9e77b8546282e
bgarhewa <bgarhewa@LT412040.asml.com>
2019-08-22 16:12:50 +0200

    Compute the set of set of confused words

--- thesis/src/main/java/refac/Injection.java
+++ thesis/src/main/java/refac/Injection.java
@@ -4,10 +4,17 @@
 package refac;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import net.automatalib.automata.fsa.impl.FastDFA;
+import net.automatalib.automata.fsa.impl.FastDFAState;
+import net.automatalib.util.automata.cover.Covers;
+import net.automatalib.words.Word;
+import utils.Misc;
 
 /**
  * 
@@ -64,9 +71,9 @@ public class Injection {
 
     /**
      * Unwrap the complicated State-Map received from the
-     * {@link StateEquivalence#calculateEquivalentStates(FastDFA, Map)} method
-     * to be {@literal Map<Integer, Integer>} since we only have one element
-     * (the product) in the map we receive.
+     * {@link stateEquivalence.StateEquivalence#calculateEquivalentStates(FastDFA, Map)}
+     * method to be {@literal Map<Integer, Integer>} since we only have one
+     * element (the product) in the map we receive.
      * 
      * @param specStateMap
      *            the
@@ -84,4 +91,91 @@ public class Injection {
         }
         return ret;
     }
+
+    /**
+     * Produces a set of a set of words that are being "confused" by the
+     * function "f". Every set in the returned set contains words that lead to
+     * states mapping to the same state in the product automaton.
+     * 
+     * @param specification
+     *            the specification dfa
+     * @param subSpecificationsList
+     *            the list of files of the sub-specifications
+     * @return set of set of confused words
+     * @throws Exception
+     */
+    public static Set<Set<Word<String>>> nonInjectiveAccessSequences(
+            FastDFA<String> specification, List<String> subSpecificationsList)
+            throws Exception {
+        // The return variable
+        Set<Set<Word<String>>> ret = new HashSet<>();
+        FastDFA<String> product = Product.computeProduct(subSpecificationsList);
+        Iterator<Word<String>> stateCoverIterator = Covers.stateCoverIterator(
+                specification, specification.getInputAlphabet());
+
+        // Map from Word -> specificationState
+        Map<Word<String>, FastDFAState> specificationAccessMap = new HashMap<>();
+
+        // Map from specificationState -> productState
+        Map<FastDFAState, FastDFAState> specificationToProductMap = new HashMap<>();
+
+        // Filling in the above two maps
+        while (stateCoverIterator.hasNext()) {
+            Word<String> input = stateCoverIterator.next();
+            FastDFAState productState = product.getState(input);
+            FastDFAState specificationState = specification.getState(input);
+            specificationAccessMap.put(input, specificationState);
+            specificationToProductMap.put(specificationState, productState);
+        }
+
+        // Inverse the specificationState -> productState map
+        // Since we are assuming that we have a non-injective mapping, we
+        // create a map from productState -> set of specificationStates.
+        Map<FastDFAState, Set<FastDFAState>> productToSpecificationMap = Misc
+                .invertMapNonUnqiue(specificationToProductMap);
+
+        // Remove the entries from the above map which map to a set of size 1.
+        // We do not care about those, as those are the injective bits.
+        removeUniqueEntries(productToSpecificationMap);
+
+        // Reverse the word -> specificationState map, which is unique
+        Map<FastDFAState, Word<String>> accessSpecificationMap = Misc
+                .invertMapUnqiue(specificationAccessMap);
+
+        // First, calculate the set of specificationStates mapped to a single
+        // product state. Next, obtain the access words of those specification
+        // states. The aforementioned access words then become a set of words
+        // which are "confused". If we combine all sets of words which are
+        // confused, we obtain the desired result.
+        for (FastDFAState productState : productToSpecificationMap.keySet()) {
+            Set<FastDFAState> specificationStatesSet = productToSpecificationMap
+                    .get(productState);
+            Set<Word<String>> confusedStates = new HashSet<>();
+            for (FastDFAState specificationState : specificationStatesSet) {
+                confusedStates
+                        .add(accessSpecificationMap.get(specificationState));
+            }
+            ret.add(confusedStates);
+        }
+        return ret;
+    }
+
+    /**
+     * Remove the injective entries present in a map of style key -> set of
+     * values
+     * 
+     * @param productToSpecificationMap
+     */
+    private static void removeUniqueEntries(
+            Map<FastDFAState, Set<FastDFAState>> productToSpecificationMap) {
+        Set<FastDFAState> thingsToRemove = new HashSet<FastDFAState>();
+        for (FastDFAState x : productToSpecificationMap.keySet()) {
+            if (productToSpecificationMap.get(x).size() == 1) {
+                thingsToRemove.add(x);
+            }
+        }
+        thingsToRemove.forEach(x -> productToSpecificationMap.remove(x));
+        return;
+    }
+
 }
