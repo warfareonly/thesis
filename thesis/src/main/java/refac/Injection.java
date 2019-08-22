65515035b2a58ae7f32b825a27e3a776815ff088
bgarhewa <bgarhewa@LT412040.asml.com>
2019-08-22 13:41:45 +0200

    Check whether or not we need a monitor
    Just the support function is added, but using it is pretty trivial to
    begin with, so no need to worry about that at the moment.

---
+++ thesis/src/main/java/refac/Injection.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package refac;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.automatalib.automata.fsa.impl.FastDFA;
+import stateEquivalence.StateEquivalence;
+
+/**
+ * @author Bharat Garhewal
+ *
+ */
+public class Injection {
+
+	/**
+	 * Given a specification and list of files of the sub-specifications, return whether a map from the set of states of
+	 * the specification to the set of states of the product is injective (true) or not (false).
+	 * 
+	 * @param specification
+	 *            specification automaton ("S")
+	 * @param subSpecificationsList
+	 *            list of files of the sub-specifications as {@literal List<String>}
+	 * @return boolean true (injective) or false (non-injective)
+	 * @throws Exception
+	 */
+	public static boolean checkInjectionFromSpecificationToProduct(FastDFA<String> specification,
+			List<String> subSpecificationsList) throws Exception {
+		FastDFA<String> product = Product.computeProduct(subSpecificationsList);
+		return checkInjectionFromSpecificationToProduct(specification, product);
+	}
+
+	/**
+	 * Given a specification and a product automaton, return whether a map from the set of states of the specification
+	 * to the set of states of the product is injective (true) or not (false).
+	 * 
+	 * @param specification
+	 *            specification automaton ("S")
+	 * @param product
+	 *            product automaton ("P")
+	 * @return boolean true (injective) or false (non-injective)
+	 */
+	private static boolean checkInjectionFromSpecificationToProduct(FastDFA<String> specification,
+			FastDFA<String> product) {
+		Map<String, FastDFA<String>> productMap = new HashMap<>(1);
+		productMap.put("product", product);
+		Map<Integer, Integer> specificationStateToProductStateMap = unwrapProductMap(
+				StateEquivalence.calculateEquivalentStates(specification, productMap));
+		return specificationStateToProductStateMap.size() == specificationStateToProductStateMap.values().stream()
+				.distinct().count();
+	}
+
+	/**
+	 * Unwrap the complicated State-Map received from the
+	 * {@link StateEquivalence#calculateEquivalentStates(FastDFA, Map)} method to be {@literal Map<Integer, Integer>}
+	 * since we only have one element (the product) in the map we receive.
+	 * 
+	 * @param specStateMap
+	 *            the {@link StateEquivalence#calculateEquivalentStates(FastDFA, Map)} map
+	 * @return a map from the set of states of the specification automaton to the set of states of the product automaton
+	 */
+	private static Map<Integer, Integer> unwrapProductMap(Map<Integer, Map<String, Integer>> specStateMap) {
+		Map<Integer, Integer> ret = new HashMap<Integer, Integer>(specStateMap.size());
+		for (Integer x : specStateMap.keySet()) {
+			ret.put(x, specStateMap.get(x).get("product"));
+		}
+		return ret;
+	}
+}
